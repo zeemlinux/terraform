eef51aebdd6b3e0f99e83b847efa08d483d90f06
zeemlinux <96556366+zeemlinux@users.noreply.github.com>
2022-11-15 22:45:51 -0500

    Delete jupiter-website-ecs directory

--- jupiter-website-ecs/backend.tf
+++
@@ -1,9 +0,0 @@
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket    = "zeeminah-terraform-remote-state"
-    key       = "jupiter-website-ecs.tfstate"
-    region    = "us-east-1"
-    profile   = "vscode"
-  }
-}
